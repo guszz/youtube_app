95d9a27ccf961d8dc3647a70df56bbe59d5681c7
Dilton Neves <diltonfilho@multiplix.net.br>
2020-06-17 09:01:24 -0300

    make carousel draggeble

--- main.js
+++ main.js
@@ -1,139 +1,88 @@
 $(document).ready(function(){
-  // iniciando a api do youtube para uma play list
-  var apiKey = 'AIzaSyCda5e6rEZcskNOp8kO2ZW05SASQwRGCzs';
+  var slider = $('.playlist_items');
+  var isDown = false;
+  var startX;
+  var scrolling;
 
-  getPlayLists();
+  $(slider).mousedown(function(e){
+    isDown = true;
+    $(this).addClass('grabbing');
+    startX = e.pageX - $(this).offset().left;
+
+    scrolling = $(this).scrollLeft();
+  });
+
+  $(slider).mouseleave(function(){
+    isDown = false;
+    $(this).removeClass('grabbing');
+  });
+
+  $(slider).mouseup(function(){
+    isDown = false;
+    $(this).removeClass('grabbing');
+  });
+
+  $(slider).mousemove(function(e){
+    if(!isDown) return;
+    e.preventDefault();
+    var x = e.pageX - $(this).offset().left;
+
+    var walk = (x - startX) * 2;
+    
+    $(this).scrollLeft(scrolling - walk);
+  });
   
-  function getPlayLists(){
-    var urlChannel = 'https://www.googleapis.com/youtube/v3/playlists';
-    var channelId = 'UCBlL-vhD7zwMLuSuDBGXSXA';
-    var channelOptions ={
-      part: 'snippet',
-      key: apiKey,
-      maxResult: 99,
-      channelId: channelId
-    };
-    $.getJSON(urlChannel, channelOptions, function(data){
-      console.log(data);
-      resultsPlayList(data);    
-    });
-  };
+  $('.scroll_btn_right').click(function(){
+    var slider_id = $(this).parents('.playlist_items').attr('id');
 
-  function mainVid(vidId){
-    $('#video .video_container').html(`
-      <iframe 
-        src="https://www.youtube.com/embed/${vidId}" 
-        frameborder="0" 
-        allow="accelerometer; 
-        autoplay; 
-        encrypted-media; 
-        gyroscope;
-        picture-in-picture" 
-        allowfullscreen
-      ></iframe>
-    `);
-  };
+    var next = $(this).parents('.playlist_items').scrollLeft() + 260;
+    $('#'+slider_id+' .playlist_items').animate({
+      scrollLeft: next 
+    }, 750);
+  });
 
-  function resultsPlayList(data){
-    $.each(data.items, function(i, listItem){
-      var title = listItem.snippet.title;
-      var listId = listItem.id;
-      $('#playlists').append(`
-        <section class="playlist">
-          <header class="playlist_header">
-            <h3 class="playlist_title">
-              ${title}
-            </h3>
-          </header>
-          <div class="playlist_items" id="${listId}">
-            <div class="scrollControl">
-              <span class="scroll_btn_left">
-                <svg class="svg-icon" viewBox="0 0 20 20">
-                  <path d="M11.739,13.962c-0.087,0.086-0.199,0.131-0.312,0.131c-0.112,0-0.226-0.045-0.312-0.131l-3.738-3.736c-0.173-0.173-0.173-0.454,
-                  0-0.626l3.559-3.562c0.173-0.175,0.454-0.173,0.626,0c0.173,0.172,0.173,0.451,0,0.624l-3.248,3.25l3.425,3.426C11.911,13.511,11.911,
-                  13.789,11.739,13.962 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,
-                  10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.148,3.374,7.521,7.521,7.521C14.147,17.521,17.521,
-                  14.148,17.521,10"></path>
-                </svg>
-              </span>
-              <span class="scroll_btn_right">
-                <svg class="svg-icon" viewBox="0 0 20 20">
-                  <path d="M12.522,10.4l-3.559,3.562c-0.172,0.173-0.451,0.176-0.625,0c-0.173-0.173-0.173-0.451,0-0.624l3.248-3.25L8.161,
-                  6.662c-0.173-0.173-0.173-0.452,0-0.624c0.172-0.175,0.451-0.175,0.624,0l3.738,3.736C12.695,9.947,12.695,10.228,12.522,
-                  10.4 M18.406,10c0,4.644-3.764,8.406-8.406,8.406c-4.644,0-8.406-3.763-8.406-8.406S5.356,1.594,10,1.594C14.643,1.594,18.406,
-                  5.356,18.406,10M17.521,10c0-4.148-3.374-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.147,
-                  17.521,17.521,14.147,17.521,10"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </section>  
-      `);
-      function getVideos(){
-        var url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-        var playlistId = listId;
-        var requestOptions = {
-          part: 'snippet',
-          key: apiKey,
-          maxResult: 99,
-          pageToken: 'CAUQAA',
-          playlistId: playlistId
-        };
-        $.getJSON(url, requestOptions, function(vdata){
-          // var vidId = vid.items[0].snippet.resourceId.videoId;
-          console.log(vdata);
-          //mainVid(vidId);
-          resultsList(vdata);
-        });
-      };
-      getVideos()
-    });
-    function resultsList(vdata){
-      $.each(vdata.items, function(i, vidItem){
-        var thumb = vidItem.snippet.thumbnails.maxres.url;
-        var title = vidItem.snippet.title;
-        var desc = vidItem.snippet.description.substr(0, 96);
-        var vid = vidItem.snippet.resourceId.videoId;
-        $('.playlist .playlist_items').append(`
-          <article class="playlist_item" data-videoKey="${vid}" >
-            <img class="thumb" src="${thumb}">
-            <div class="detail">
-              <h4 class="detail_title">${title}</h4>
-              <p class="detail_desc">
-                ${desc}...
-              </p>
-            </div>
-          </article>
-        `);
-      });
+  $('.scroll_btn_left').click(function(){
+    var prev = $(this).parents('.playlist_items').scrollLeft() - 260;
+    $('.playlist_items').animate({
+      scrollLeft: prev
+    }, 750);
+  });
 
-      $('.playlist .playlist_items').on('click','article', function(){
-        var vidId = $(this).attr('data-videoKey');
-        mainVid(vidId);
-      });
-    };
+  if($('#youtube_api').length > 0 && screen.width > 768){
+    var slider = $('.lists_container');
+    var sliderItem = $('.lists_container .items_container');
+    var isDown = false;
+    var startX;
+    var scrolling;
 
-    $('.scroll_btn_right').click(function(){
-      var next = $(this).parents('.playlist_items').scrollLeft() + 260;
-      $('.playlist_items').animate({
-        scrollLeft: next 
-      }, 750);
+    $(slider).mousedown(function(e){
+      e.preventDefault();
+      isDown = true;
+      $(this).addClass('grabbing');
+      startX = e.pageX - $(this).offset().left;
+
+      scrolling = $(this).scrollLeft();
     });
 
-    $('.scroll_btn_left').click(function(){
-      var prev = $(this).parents('.playlist_items').scrollLeft() - 260;
-      $('.playlist_items').animate({
-        scrollLeft: prev
-      }, 750);
+    $(slider).mouseleave(function(){
+      isDown = false;
+      $(this).removeClass('grabbing');
     });
 
-    $('.playlist_items').scroll(function(){
-      if($(this).scrollLeft() > 5){
-        $('.scroll_btn_left').addClass('show');
-      }
-      else{
-        $('.scroll_btn_left').removeClass('show');
-      }
+    $(slider).mouseup(function(e){
+      e.preventDefault();
+      isDown = false;
+      $(this).removeClass('grabbing');
     });
-  };
+
+    $(slider).mousemove(function(e){
+      if(!isDown) return;
+      e.preventDefault();
+      var x = e.pageX - $(this).offset().left;
+
+      var walk = (x - startX) * 2;
+      
+      $(this).scrollLeft(scrolling - walk);
+    });
+  }
 });
